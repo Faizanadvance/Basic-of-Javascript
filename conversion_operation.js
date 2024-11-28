1a0bf81bd41447bb33d583760d1e9f8a13045705
Faizanadvance <professionaltarget12@gmail.com>
2024-11-28 19:23:50 +0500

    Operations

--- conversion_operation.js
+++ conversion_operation.js
@@ -1,12 +1,12 @@
-let x = 32
+// let x = 32
 
-console.log(typeof x)
+// console.log(typeof x)
 
-let y = String(x)    // Convert the datatypes 
-let z = Number(y)
-console.log(typeof y)
-console.log(y)
-console.log(typeof z)
+// let y = String(x)    // Convert the datatypes 
+// let z = Number(y)
+// console.log(typeof y)
+// console.log(y)
+// console.log(typeof z)
 
 
 // "32" => 32
@@ -14,11 +14,35 @@ console.log(typeof z)
 // true => 1
 // false => 0
 
-let bool = "e"
-let bool2 = Boolean(bool)
-console.log(bool2)
+// let bool = "e"
+// let bool2 = Boolean(bool)
+// console.log(bool2)
 
 // 1 => true
 // 0 => false
 // "" => false
 // "abc" => true
+
+
+//  **************************  Operations  **************************
+
+let a = 10 
+let b = -a
+console.log(b)
+
+console.log(2**3)   // **=^ power
+console.log("1" + 2 +3)
+console.log("2" - 2 - 3)
+console.log(1+2+3+"4")
+console.log(typeof 1*2*3*"4"%5)
+console.log(+true)
+console.log(-true)
+console.log(+false)
+
+let x,y,z
+x=y=z=2+2
+console.log(x,y,z)
+
+d =100
+d++
+console.log(d)
